8f39eaf34f8a8a1b5aad10c24a2feaad499ab96e
Annargb <annasamoylenko2015@gmail.com>
2023-09-10 20:04:37 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  setTimeout(changeColor, 0);
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
