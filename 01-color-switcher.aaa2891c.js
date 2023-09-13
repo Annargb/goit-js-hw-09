10e6e19ba1f26f97bc0eae3fad4fb69ded9de8c5
Annargb <130927958+Annargb@users.noreply.github.com>
2023-09-13 14:47:07 +0000

    Deploying to gh-pages from @ Annargb/goit-js-hw-09@96ef73c50213de837e3502e6289486463afb5540 🚀

--- 01-color-switcher.aaa2891c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,setTimeout(a,0),o=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.aaa2891c.js.map
